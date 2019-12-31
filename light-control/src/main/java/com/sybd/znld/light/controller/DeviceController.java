b7207abf325891ab9ac414c8e8a39211e7137444
xw <xw@sybd.com>
2019-12-31 10:49:49 +0800

    update

--- light-control/src/main/java/com/sybd/znld/light/controller/DeviceController.java
+++ light-control/src/main/java/com/sybd/znld/light/controller/DeviceController.java
@@ -1,19 +1,23 @@
 package com.sybd.znld.light.controller;
 
+import com.sybd.znld.mapper.lamp.OneNetResourceMapper;
+import com.sybd.znld.model.environment.RealTimeData;
 import com.sybd.znld.model.lamp.dto.LampStatistic;
 import com.sybd.znld.model.lamp.dto.RegionBoxLamp;
 import com.sybd.znld.light.service.IDeviceService;
 import com.sybd.znld.model.lamp.dto.Message;
 import com.sybd.znld.model.lamp.dto.OpResult;
+import com.sybd.znld.model.onenet.Config;
 import com.sybd.znld.model.onenet.OneNetKey;
+import com.sybd.znld.model.onenet.dto.BaseResult;
 import com.sybd.znld.model.onenet.dto.CommandParams;
+import com.sybd.znld.service.lamp.ILampService;
 import com.sybd.znld.service.onenet.IOneNetService;
-import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import com.sybd.znld.util.MyString;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,11 +30,20 @@ import java.util.List;
 public class DeviceController implements IDeviceController {
     private final IDeviceService deviceService;
     private final IOneNetService oneNetService;
+    private final ILampService lampService;
+    private final RedissonClient redissonClient;
+    private final OneNetResourceMapper oneNetResourceMapper;
 
     @Autowired
-    public DeviceController(IDeviceService deviceService, IOneNetService oneNetService) {
+    public DeviceController(IDeviceService deviceService,
+                            IOneNetService oneNetService,
+                            ILampService lampService, RedissonClient redissonClient,
+                            OneNetResourceMapper oneNetResourceMapper) {
         this.deviceService = deviceService;
         this.oneNetService = oneNetService;
+        this.lampService = lampService;
+        this.redissonClient = redissonClient;
+        this.oneNetResourceMapper = oneNetResourceMapper;
     }
 
     @Override
@@ -51,6 +64,119 @@ public class DeviceController implements IDeviceController {
             log.error(ExceptionUtils.getStackTrace(ex));
         }
         return null;
+    }
+
+    @Override
+    public OpResult offDevice(String imei, String dataStream) {
+        if(MyString.isEmptyOrNull(imei) || MyString.isEmptyOrNull(dataStream)) {
+            log.error("参数错误");
+            return null;
+        }
+        var dataStreamId = dataStream;
+        var dataStreamName = dataStream;
+        if(!OneNetKey.isDataStreamId(dataStream)){ // 是资源名称
+            dataStreamId = this.lampService.getDataStreamIdByResourceName(dataStream);
+            if(MyString.isEmptyOrNull(dataStreamId)){
+                log.error("错误的资源名称["+dataStream+"]");
+                return null;
+            }
+        }
+        if(OneNetKey.isDataStreamId(dataStream)){ // 是资源id
+            var resource = this.oneNetResourceMapper.selectByOneNetKey(OneNetKey.from(dataStream));
+            if(resource == null){
+                log.error("错误的资源id["+dataStream+"]");
+                return null;
+            }
+            dataStreamName = resource.description;
+
+        }
+        var cmd = new CommandParams();
+        cmd.imei = imei;
+        cmd.oneNetKey = OneNetKey.from(dataStreamId);
+        cmd.command = "off";
+        var ret = this.oneNetService.execute(cmd);
+        if(ret.isOk()) {
+            var map = this.redissonClient.getMap(Config.getRedisRealtimeKey(imei));
+            var data = (RealTimeData) map.get(dataStreamName);
+            data.value = false;
+            map.put(dataStreamName, data);
+        }
+        var map = new HashMap<String, BaseResult>();
+        map.put(dataStream, ret);
+        var result = new OpResult();
+        result.values = map;
+        result.code = 0;
+        result.msg = "";
+        return result;
+    }
+
+    @Override
+    public OpResult onDevice(String imei, String dataStream) {
+        if(MyString.isEmptyOrNull(imei) || MyString.isEmptyOrNull(dataStream)) {
+            log.error("参数错误");
+            return null;
+        }
+        var dataStreamId = dataStream;
+        var dataStreamName = dataStream;
+        if(!OneNetKey.isDataStreamId(dataStream)){ // 可能是资源名称
+            dataStreamId = this.lampService.getDataStreamIdByResourceName(dataStream);
+            if(MyString.isEmptyOrNull(dataStreamId)){
+                log.error("错误的资源名称["+dataStream+"]");
+                return null;
+            }
+        }
+        if(OneNetKey.isDataStreamId(dataStream)){ // 是资源id
+            var resource = this.oneNetResourceMapper.selectByOneNetKey(OneNetKey.from(dataStream));
+            if(resource == null){
+                log.error("错误的资源id["+dataStream+"]");
+                return null;
+            }
+            dataStreamName = resource.description;
+
+        }
+        var cmd = new CommandParams();
+        cmd.imei = imei;
+        cmd.oneNetKey = OneNetKey.from(dataStreamId);
+        cmd.command = "on";
+        var ret = this.oneNetService.execute(cmd);
+        if(ret.isOk()) {
+            var map = this.redissonClient.getMap(Config.getRedisRealtimeKey(imei));
+            var data = (RealTimeData) map.get(dataStreamName);
+            data.value = true;
+            map.put(dataStreamName, data);
+        }
+        var map = new HashMap<String, BaseResult>();
+        map.put(dataStream, ret);
+        var result = new OpResult();
+        result.values = map;
+        result.code = 0;
+        result.msg = "";
+        return result;
+    }
+
+    @Override
+    public Boolean isDeviceOn(String imei, String dataStream) {
+        if(MyString.isEmptyOrNull(imei) || MyString.isEmptyOrNull(dataStream)) {
+            log.error("参数错误");
+            return null;
+        }
+        var map = this.redissonClient.getMap(Config.getRedisRealtimeKey(imei));
+        if(map == null || map.isEmpty()) return null;
+        RealTimeData result = null;
+        var dataStreamId = dataStream;
+        if(OneNetKey.isDataStreamId(dataStream)){ // 如果是资源id
+            dataStreamId = this.lampService.getDataStreamIdByResourceName(dataStream);
+            var resource = this.oneNetResourceMapper.selectByOneNetKey(OneNetKey.from(dataStreamId));
+            if(resource == null){
+                return null;
+            }
+            result = (RealTimeData) map.get(resource.description);
+
+        } else {
+            result = (RealTimeData) map.get(dataStream);
+        }
+        if(result == null) return null;
+        return (Boolean) result.value;
     }
 
     @Override
--- light-control/src/main/java/com/sybd/znld/light/controller/IDeviceController.java
+++ light-control/src/main/java/com/sybd/znld/light/controller/IDeviceController.java
@@ -13,12 +13,20 @@ import java.util.List;
 
 public interface IDeviceController {
     // 打开关闭设备电源
-    @PutMapping(value = "status/{imei}/{data_stream}/{action}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @PutMapping(value = "power/{imei}/{data_stream}/{action}", produces = {MediaType.APPLICATION_JSON_VALUE})
     OpResult operateDevice(@PathVariable("imei") String imei,
                            @PathVariable(name = "data_stream") String dataStream,
                            @PathVariable("action") Message.CommonAction action);
+    // 关闭设备电源（旧版）
+    @PutMapping(value = "power/off/{imei}/{data_stream}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    OpResult offDevice(@PathVariable("imei") String imei, @PathVariable(name = "data_stream") String dataStream);
+    // 打开设备电源（旧版）
+    @PutMapping(value = "power/on/{imei}/{data_stream}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    OpResult onDevice(@PathVariable("imei") String imei, @PathVariable(name = "data_stream") String dataStream);
 
     // 查询设备开关状态
+    @GetMapping(value = "power/{imei}/{data_stream}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    Boolean isDeviceOn(@PathVariable("imei") String imei, @PathVariable(name = "data_stream") String dataStream);
 
     // 获取某个区域下的所有路灯
     @GetMapping(value = "lamp/{organ_id}", produces = {MediaType.APPLICATION_JSON_VALUE})
